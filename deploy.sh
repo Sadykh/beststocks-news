717aeedaee6c154510f2e448d5afb08dd46d9180
Sadykh <sadykh.sadykhov@ya.ru>
2020-08-31 18:08:31 +0300

    Fix deploy

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@
 docker-compose up -d
 ./composer.sh install
 docker-compose exec php chown -R www-data:www-data /var/www/html/web/assets
+docker-compose exec php chown -R www-data:www-data /var/www/html/web/files
 docker-compose exec php chown -R www-data:www-data /var/www/html/runtime
 sleep 30
 #docker-compose exec db sh -c 'echo "ALTER DATABASE yii CHARACTER SET utf8 COLLATE utf8_general_ci;" | mysql'
